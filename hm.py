43586641c792adf6792b8fa4aa172e7073ec3475
tiger20111 <nikit-proc@mail.ru>
2020-12-17 15:18:19 +0300

    upload homework

--- hm.py
+++ hm.py
@@ -12,7 +12,7 @@ df.drop(df.columns[[0, 4, 5, 9, 10, 11]],
 y = df['rarity']
 X = df.drop(columns=['rarity'])
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=23)
 
 logmodel = LogisticRegression()
 logmodel.fit(X_train, y_train)
